76b6605675c665a3c98d08bff034f8cd0ed6c517
junseong <47383111+junseongday@users.noreply.github.com>
2023-06-26 13:32:51 +0900

    Update selenium_get_text1.py

--- ch4/selenium_get_text1.py
+++ ch4/selenium_get_text1.py
@@ -1,18 +1,44 @@
+# from selenium import webdriver
+# from selenium.webdriver.chrome.options import Options
+# import time
+# from _MyPath import URL, DRIVER as d
+
+# # 책 소개 사이트 열기 --- (1)
+# driver = webdriver.Chrome(d)
+# driver.get(URL)
+
+# # h1 요소의 텍스트 출력하기 --- (2)
+# ele = driver.find_element_by_tag_name('h1')
+# print('h1:', ele.text)
+
+# # id가 cnt인 요소의 텍스트 출력하기 --- (3)
+# ele = driver.find_element_by_id('cnt')
+# print('#cnt:', ele.text)
+
+# time.sleep(3)
+# driver.quit()
+
+
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service as ChromeService
 import time
 from _MyPath import URL, DRIVER as d
+from selenium.webdriver.common.by import By
 
 # 책 소개 사이트 열기 --- (1)
-driver = webdriver.Chrome(d)
+options = webdriver.ChromeOptions()
+options.add_experimental_option("excludeSwitches", ["enable-logging"])
+service = ChromeService(executable_path=d)
+driver = webdriver.Chrome(service=service, options=options)
 driver.get(URL)
 
 # h1 요소의 텍스트 출력하기 --- (2)
-ele = driver.find_element_by_tag_name('h1')
+ele = driver.find_element(By.CSS_SELECTOR, 'h1')
 print('h1:', ele.text)
 
 # id가 cnt인 요소의 텍스트 출력하기 --- (3)
-ele = driver.find_element_by_id('cnt')
+ele = driver.find_element(By.CSS_SELECTOR,'#cnt')
 print('#cnt:', ele.text)
 
 time.sleep(3)
